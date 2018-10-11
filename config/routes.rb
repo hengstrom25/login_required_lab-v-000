43614aca949aa3bfbefbd9f422e42df3cb8b1050
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-11 19:01:56 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'  
   root 'application#hello'
+  get '/secrets' => 'secrets#show'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
