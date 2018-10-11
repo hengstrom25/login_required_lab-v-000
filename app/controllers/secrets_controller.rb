775cb13059e6a1dfb897ca690bee68d00af07e60
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-11 18:57:35 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,21 @@
 class SecretsController < ApplicationController
-  
+  before_action :require_login
+ 
   def show
     @document = Document.find(params[:id])
+  end
+ 
+  def index
+  end
+ 
+  def create
+    @document = Document.create(author_id: user_id)
+  end
+ 
+  private
+ 
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
   end
   
 end
