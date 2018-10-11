9991bdb7292a1abdff49c9adf663a15700301108
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-11 19:05:33 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
       session[:name] = params[:name]
         if session[:name] == nil || session[:name].empty?
-          redirect_to sessions_new_path
+          redirect_to login_path
         else
           redirect_to '/secrets/show'
         end
