d2cb850debf56d0a486c50d540c60617505731b7
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-11 19:09:22 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
  
-    def create
-        if params[:name] == nil || params[:name].empty?
-          redirect_to login_path
-        else
-          session[:name] = params[:name]
-          render '/secrets/show'
-        end
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      render '/secrets/show'
     end
+  end
     
     def destroy
       session.delete :name
